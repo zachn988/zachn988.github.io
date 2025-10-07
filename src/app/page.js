73945a7ae5621c79971379c9f96e3aff1bdd4acb
zachn988 <zachnalepa@gmail.com>
2025-10-07 02:22:49 -0500

    stylesheets, more like stylesheesh

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 import RootLayout from "./layout.js";
-import "./page.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   return <div>
     <div>
-        <div className="address_bar">
-            <p className="address">Address</p>
-            <p className="links">Blog &gt; Inferno &gt; Canto 1</p>
+        <div className={styles.address_bar}>
+            <p className={styles.address}>Address</p>
+            <p className={styles.links}>Blog &gt; Inferno &gt; Canto 1</p>
         </div>
-        <div className="post_border">
-            <p className="post">
+        <div className={styles.post_border}>
+            <p className={styles.post}>
                 September 29 2025 <br></br><br></br>
                 In the middle of the journey of our life I came to myself in a dark wood where the
                 straight way was lost. Ah! how hard a thing it is to tell what a wild,
@@ -18,13 +18,13 @@ export default function Home() {
                 to treat of the good that I ...
             </p>
         </div>
-        <hr></hr>
-        <div className="address_bar">
-            <p className="address">Address</p>
-            <p className="links">Blog &gt; Inferno &gt; Canto 1</p>
+        <hr className={styles.hr}></hr>
+        <div className={styles.address_bar}>
+            <p className={styles.address}>Address</p>
+            <p className={styles.links}>Blog &gt; Inferno &gt; Canto 1</p>
         </div>
-        <div className="post_border">
-            <p className="post">
+        <div className={styles.post_border}>
+            <p className={styles.post}>
                 September 29 2025 <br></br><br></br>
                 In the middle of the journey of our life I came to myself in a dark wood where the
                 straight way was lost. Ah! how hard a thing it is to tell what a wild,
